4f1a32ae86cc28d5e594206f09f7bfebc8f525fb
Victor Ponce <vicmanuelr@gmail.com>
2022-08-11 11:33:32 -0600

    Fixed formatting of data.py

--- data.py
+++ data.py
@@ -4,9 +4,7 @@ question_data = [
     {"text": "Approximately one quarter of human bones are in the feet.", "answer": "True"},
     {"text": "The total surface area of a human lungs is the size of a football pitch.", "answer": "True"},
     {"text": "In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home "
-             "to eat.",
-     "answer": "True"
-     },
+             "to eat.", "answer": "True"},
     {"text": "In London, UK, if you happen to die in the House of Parliament, you are entitled to a state funeral.",
      "answer": "False"},
     {"text": "It is illegal to pee in the Ocean in Portugal.", "answer": "True"},
